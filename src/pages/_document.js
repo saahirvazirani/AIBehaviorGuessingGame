4daee0e3ba32efe17af72f3fa2defa727182e8be
Saahir <103967382+saahirvazirani@users.noreply.github.com>
2024-08-25 15:20:32 -0400

    document fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-// _document.js in the pages folder
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -6,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" defer></script> {/* or use async */}
       </Head>
       <body>
         <Main />
